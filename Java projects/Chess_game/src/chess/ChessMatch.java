2171c295f16d62d40ac102b5d13efdec4d45b6b7
= <=>
2023-07-07 17:15:19 -0300

    Projeto até a aula 187

---
+++ Java projects/Chess_game/src/chess/ChessMatch.java
@@ -0,0 +1,32 @@
+package chess;
+
+import boardgame.Board;
+import chess.pieces.King;
+
+public class ChessMatch {
+	private Board board;
+
+	public ChessMatch() {
+		board = new Board(8, 8);
+		initialSetup();
+	}
+
+	public ChessPiece[][] getPieces() {
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
+			}
+		}
+		return mat;
+	}
+
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).ToPosition());
+	}
+	
+	private void initialSetup() {
+		placeNewPiece('e', 8 ,new King(board, Color.BLACK));
+		placeNewPiece('e', 1 , new King(board, Color.WHITE));
+	}
+}
