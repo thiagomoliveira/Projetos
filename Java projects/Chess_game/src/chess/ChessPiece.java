744a91bad0c193eb09c1c3d60fc1f58e7797bc24
= <=>
2023-07-11 15:24:10 -0300

    Pawn

--- Java projects/Chess_game/src/chess/ChessPiece.java
+++ Java projects/Chess_game/src/chess/ChessPiece.java
@@ -16,16 +16,19 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
-	
+
+	public int getMoveCount() {
+		return moveCount;
+	}
+
 	public void increaseMoveCount() {
-		moveCount ++;
+		moveCount++;
 	}
-	
+
 	public void decreaseMoveCount() {
-		moveCount --;
+		moveCount--;
 	}
-	
-	
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
