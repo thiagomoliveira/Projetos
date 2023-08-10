c8436adec1ac265ba86a5b40884da932c0f663b1
Thiago Martins <pessoal.thiagomartins@gmail.com>
2023-08-10 18:29:26 -0300

    entidades Seller e Department

---
+++ Java projects/demo_dao_jdbc/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+	}
+
+}
