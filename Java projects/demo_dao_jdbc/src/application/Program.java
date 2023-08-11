050310302e856a5297476b10951414bd192dac1c
Thiago Martins <pessoal.thiagomartins@gmail.com>
2023-08-11 13:12:02 -0300

    FindById implementação

--- Java projects/demo_dao_jdbc/src/application/Program.java
+++ Java projects/demo_dao_jdbc/src/application/Program.java
@@ -1,14 +1,16 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.FindById(3);
+		System.out.println(seller);
 	}
 
 }
