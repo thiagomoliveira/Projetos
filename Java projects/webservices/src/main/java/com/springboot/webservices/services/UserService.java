d8693429f2f97c00b81913adb27654a26b0a7dd7
Thiago Martins <pessoal.thiagomartins@gmail.com>
2023-08-29 20:39:29 -0300

    Exception handling - delete

--- Java projects/webservices/src/main/java/com/springboot/webservices/services/UserService.java
+++ Java projects/webservices/src/main/java/com/springboot/webservices/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.springboot.webservices.entities.User;
@@ -30,7 +32,11 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
